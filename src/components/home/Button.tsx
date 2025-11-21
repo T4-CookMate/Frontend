a63310ce1349994f7792bb9d55a62d90f6d4f0f9
minddoi <minddoi493@gmail.com>
2025-11-21 22:17:42 +0900

    feat: HomePage UI 작업

---
+++ src/components/home/Button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    width: 349px;
+    min-height: 60px;
+    flex-shrink: 0;
+    border-radius: 28px;
+    /* padding: 16px 20px; // */
+    background: #f3c11b;
+    border: none;
+    cursor: pointer;
+
+    justify-content: center;          /* 텍스트 가운데 정렬 */
+    text-align: center;               /* 여러 줄일 때 중앙 정렬 */
+    word-break: keep-all;
+    white-space: normal; 
+    
+
+    color: #000;
+    text-align: center;
+    font-family: "KoddiUD OnGothic", sans-serif;
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%; /* 27px */
+    letter-spacing: -0.36px;
+
+    transition: background 0.2s ease;
+
+    &:hover {
+        background: #f0b800; /* 살짝 어두운 노란색 */
+    }
+
+    &:active {
+        background: #d9a800;
+    }
+`;
