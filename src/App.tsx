03924118d817754447471d2804ac5670afad2654
minddoi <minddoi493@gmail.com>
2025-11-21 23:03:52 +0900

    refactor: 홈 페이지 및 글로벌 컴포넌트 라우팅 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import { AppLayout } from "@components/layout/AppLayout";
 import OnboardingSplash from "@pages/OnboardingSplash";
 import HomePage from "@pages/HomePage";
 import LoginPage from "@pages/LoginPage";
 import SearchPage from "@pages/SearchPage";
+import RootLayout from "layouts/RootLayout";
 
 export default function App() {
   return (
-    <AppLayout>
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/onboarding" replace />} /> */}
+    <Routes>
+      <Route element={<RootLayout />}>
         <Route path="/onboarding" element={<OnboardingSplash />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </AppLayout>
+      </Route>
+    </Routes>
   )
 }
