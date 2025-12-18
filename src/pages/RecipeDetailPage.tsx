42ccc009cfcaf70ddff3da693b304acf0f861090
minddoi <minddoi493@gmail.com>
2025-12-19 08:36:52 +0900

    feat: 에러 및 레시피 더미 데이터 정리

--- src/pages/RecipeDetailPage.tsx
+++ src/pages/RecipeDetailPage.tsx
@@ -246,14 +246,14 @@ export default function RecipeDetailPage() {
 
     const entryRef = useRef<HTMLHeadingElement>(null);
 
-    // useEffect(() => {
-    //     const onScroll = () => {
-    //         setScrolled(window.scrollY > 10)
-    //     }
+    useEffect(() => {
+        const onScroll = () => {
+            setScrolled(window.scrollY > 10)
+        }
 
-    //     window.addEventListener('scroll', onScroll)
-    //     return () => window.removeEventListener('scroll', onScroll)
-    // }, [])
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
 
     useEffect(() => {
         const t = setTimeout(() => {
